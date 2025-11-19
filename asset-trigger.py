25db97b30cc92da9867453c9e16bc29981f35f62
CHarris-VR <charrisagent@gmail.com>
2025-11-18 20:16:38 -0500

    Added input risk and input as int to test the system

--- asset-trigger.py
+++ asset-trigger.py
@@ -9,3 +9,17 @@ print("--- Asset Information ---")
 print("Analyst:", analyst_name)
 print("System:", system_name)
 print("Criticality:", criticality)
+
+# Input risk number
+
+risk_score = input("Enter a risk score (1-10): ")
+print("Risk score entered:", risk_score)
+print("Data type of risk_score:", type(risk_score))
+
+
+
+# Output risk
+
+risk_score = int(risk_score)
+print("Numeric risk score:", risk_score)
+print("Data type now:", type(risk_score))
