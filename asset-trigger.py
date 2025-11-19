6e4664483d180b138e6fc1509e6c2e0c551deccc
CHarris-VR <charrisagent@gmail.com>
2025-11-18 20:08:19 -0500

    thrid commit, changing from static to input methods for the tool

--- asset-trigger.py
+++ asset-trigger.py
@@ -1,10 +1,11 @@
 # Week 2 - Working with Variables
 # Author: Curtis Harris
 
-analyst_name = "Jordan"
-system_name = "workstation-01"
-criticality = "high"
+analyst_name = input("Enter your name: ")
+system_name = input("Enter system hostname: ")
+criticality = input("Enter system criticality (low, medium, high): ")
 
+print("--- Asset Information ---")
 print("Analyst:", analyst_name)
 print("System:", system_name)
-print("Criticality Level:", criticality)
+print("Criticality:", criticality)
