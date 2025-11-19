3a2e4f58ecad2f48e2c8eabaaf029e32679d9920
CHarris-VR <charrisagent@gmail.com>
2025-11-18 20:53:57 -0500

    Testing how to change colors for my code

---
+++ testcolor.py
@@ -0,0 +1,15 @@
+# Testing syntax for color
+
+print("\033[31m" "Testing"])
+
+# Creating system for asset_report.py 
+
+risk = input("Enter the Risk Score(1-10): ")
+risk_int = int(risk)
+
+if risk_int >= 8:
+    print("\033[31m" "High-Risk Asset – prioritize review.")
+elif risk_int >= 5:
+    print("\033[33m" "Medium-Risk Asset – monitor closely.")
+else:
+    print("\033[37m" "Low-Risk Asset – standard monitoring.")
