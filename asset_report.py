d9aa6ac79880b5270cbf1588aac3afbd8d9299b4
CHarris-VR <charrisagent@gmail.com>
2025-11-18 20:33:41 -0500

    building the intro part of the Asset Report

---
+++ asset_report.py
@@ -0,0 +1,12 @@
+# Week 2 Challenge
+# Author - Curtis Harris
+
+analyst_name = input("Enter your name: ")
+Department = input("Enter your Department: ")
+system_name = input("Enter system hostname: ")
+criticality = input("Enter system criticality (low, medium, high): ")
+risk = input("Enter the Risk Score(1-10): ")
+
+print ("=" * 40)
+print ("Cyber Defense - Asset Report")
+print ("=" * 40)
