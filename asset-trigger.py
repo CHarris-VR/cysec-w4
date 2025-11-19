86558973f67a305b4f23b474181048ccba7955ac
CHarris-VR <charrisagent@gmail.com>
2025-11-18 20:22:47 -0500

    added decision making logic

--- asset-trigger.py
+++ asset-trigger.py
@@ -13,13 +13,18 @@ print("Criticality:", criticality)
 # Input risk number
 
 risk_score = input("Enter a risk score (1-10): ")
-print("Risk score entered:", risk_score)
-print("Data type of risk_score:", type(risk_score))
 
-
-
-# Output risk
+# Output risk logic and convernt from string to int
 
 risk_score = int(risk_score)
 print("Numeric risk score:", risk_score)
 print("Data type now:", type(risk_score))
+
+# Decision making logic addition based on risk score
+
+if risk_score >= 8:
+    print("[*] High-Risk Asset – prioritize review.")
+elif risk_score >= 5:
+    print("[!] Medium-Risk Asset – monitor closely.")
+else:
+    print("[+] Low-Risk Asset – standard monitoring.")
